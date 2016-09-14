9f789cae544192c37608925e4b83667570d0b9ce
Ryan <tulus.ryan@gmail.com>
2016-09-14 15:52:18 +0700

    mux

--- app.go
+++ app.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"log"
+	"github.com/goji/goji"
+	"github.com/rtulus/togel/src/nomor"
 )
 
 func main() {
 	log.Println("Hello World");
+
+	mux := goji.NewMux()
+	mux.HandleFuncC(pat.Get("togel/new"), nomor.New)
+
+	http.ListenAndServe("localhost:3434", mux)
 }
 
 
