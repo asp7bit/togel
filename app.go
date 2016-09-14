593db46cf0f84b281002410f88200e79fb080937
Ryan <tulus.ryan@gmail.com>
2016-09-14 15:58:05 +0700

    remove goji

--- app.go
+++ app.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"log"
-	"github.com/goji/goji"
-	"github.com/rtulus/togel/src/nomor"
+#	"github.com/goji/goji"
+#	"github.com/rtulus/togel/src/nomor"
 )
 
 func main() {
 	log.Println("Hello World");
 
-	mux := goji.NewMux()
-	mux.HandleFuncC(pat.Get("togel/new"), nomor.New)
+#	mux := goji.NewMux()
+#	mux.HandleFuncC(pat.Get("togel/new"), nomor.New)
 
-	http.ListenAndServe("localhost:3434", mux)
+#	http.ListenAndServe("localhost:3434", mux)
 }
 
 
